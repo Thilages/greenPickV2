315a87a2ac26e10df5a561ebf7062fea911d2a34
Thilagesh <thilagesh030205@gmail.com>
2024-11-14 13:10:57 +0530

    adding domain

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='shop' element={<Shop />}/>
+      <Route path='/shop' element={<Shop />}/>
     </Routes>
   )
 }
